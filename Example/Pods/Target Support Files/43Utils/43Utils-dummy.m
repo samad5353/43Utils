d9a2dd2fd7fd1d12c7f2c30337ebc2f5e9d77b94
Abdul Samad <samad@iMac160.local>
2018-04-30 10:44:04 +0400

    initialCommit

---
+++ Example/Pods/Target Support Files/43Utils/43Utils-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_43Utils : NSObject
+@end
+@implementation PodsDummy_43Utils
+@end
---
+++ Example/Pods/Target Support Files/Pods-43Utils_Tests/Pods-43Utils_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_43Utils_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_43Utils_Tests
+@end
